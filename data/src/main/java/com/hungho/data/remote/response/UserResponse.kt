f2b8099d247f7db6547712721b0a4a02390be063
Hung Ho <hungho0206.it@gmail.com>
2025-05-07 20:28:12 +0700

    Remove Unused Function

--- data/src/main/java/com/hungho/data/remote/response/UserResponse.kt
+++ data/src/main/java/com/hungho/data/remote/response/UserResponse.kt
@@ -3,7 +3,6 @@ package com.hungho.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 import com.hungho.data.local.database.entity.UserEntity
-import com.hungho.domain.model.UserModel
 
 internal data class UserResponse(
     @SerializedName("id")
@@ -19,16 +18,6 @@ internal data class UserResponse(
         return UserEntity(
             id = this.id ?: 0,
             loginUsername = this.login.orEmpty(),
-            avatarUrl = this.avatarUrl.orEmpty(),
-            htmlUrl = this.htmlUrl.orEmpty()
-        )
-    }
-
-    fun toUserModel(): UserModel? {
-        if (this.id == null || this.login == null) return null
-        return UserModel(
-            id = this.id,
-            username = this.login,
             avatarUrl = this.avatarUrl.orEmpty(),
             htmlUrl = this.htmlUrl.orEmpty()
         )
