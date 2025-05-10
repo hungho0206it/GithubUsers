486ef47a463e99e1552668ce87abf1b3a7eb5f2a
Hung Ho <hungho0206.it@gmail.com>
2025-05-10 23:42:54 +0700

    Fix DI Issue

--- app/src/main/java/com/hungho/githubusers/ui/feature/user_detail/UserDetailsViewModel.kt
+++ app/src/main/java/com/hungho/githubusers/ui/feature/user_detail/UserDetailsViewModel.kt
@@ -6,23 +6,22 @@ import com.hungho.domain.model.UserDetailsModel
 import com.hungho.domain.usecase.GetUserDetailsUseCase
 import com.hungho.githubusers.ui.base.BaseViewModel
 import com.hungho.githubusers.ui.utils.custom.SingleLiveData
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlin.coroutines.CoroutineContext
 
 class UserDetailsViewModel(
     private val getUserDetailsUseCase: GetUserDetailsUseCase,
-    private val coroutineContext: CoroutineContext,
-) :
-    BaseViewModel() {
+    private val coroutineDispatcher: CoroutineDispatcher,
+) : BaseViewModel() {
     val userDetails = SingleLiveData<UserDetailsModel>()
 
     fun getUserDetails(username: String) {
         setLoading(true)
         getUserDetailsUseCase(username)
-            .flowOn(coroutineContext)
+            .flowOn(coroutineDispatcher)
             .catch {
                 setLoading(false)
                 error.postValue(it.toFailure())
